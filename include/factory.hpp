4e5fcc0849ffa1928bf5bf09280b6b6b4c774046
P-franczak <147486694+P-franczak@users.noreply.github.com>
2023-12-22 21:11:14 +0100

    Update factory.hpp
    Definicja szablonu klasy NodeCollection

--- include/factory.hpp
+++ include/factory.hpp
@@ -1,16 +1,33 @@
 #ifndef NETSIM_FACTORY_HPP
 #define NETSIM_FACTORY_HPP
 
+#include "nodes.hpp"
+#include <vector>
 
-template <class Worker>
+template <class Node>
 class NodeCollection {
 public:
-    void add(Node &&node);
-    void remove_by_id(ElementID id)
-    NodeCollection<Node>::iterator find_by_id(ElementID id)
-    NodeCollection<Node>::const_iterator {query} find_by_id(ElementID id)
-private:
+    using container_t = typename std::list<Node>;
+    using iterator = typename container_t::iterator;
+    using const_iterator = typename container_t::const_iterator;
 
+    void add(Node&& node) { collection_.emplace_back(std::move(node)); }
+
+    iterator begin() { return collection_.begin(); }
+    iterator end() { return collection_.end(); }
+
+    const_iterator begin() const { return collection_.begin(); }
+    const_iterator end() const { return collection_.end(); }
+
+    const_iterator cbegin() const { return collection_.begin(); }
+    const_iterator cend() const { return collection_.end(); }
+
+    iterator find_by_id(ElementID id) { return std::find_if(begin(), end(), [id](auto node){ return node.get_id() == id; } ); }
+    const_iterator find_by_id(ElementID id) const { return std::find_if(cbegin(), cend(), [id](Node node){ return node.get_id() == id; }); }
+
+    void remove_by_id(ElementID id) { auto foundID = find_by_id(id); if(foundID != end()) collection_.erase(foundID); }
+private:
+    container_t collection_;
 };
 
 
