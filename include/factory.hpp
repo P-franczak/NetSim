88e3802422ef1eb1aba50ca2848233d080f2f3f0
czyzyk20 <128053440+czyzyk20@users.noreply.github.com>
2023-12-22 21:20:59 +0100

    Update factory.hpp

--- include/factory.hpp
+++ include/factory.hpp
@@ -32,20 +32,50 @@ private:
 
 
 
+
 class Factory{
 public:
-    void add_ramp(&&Ramp)
-    void remove_ramp(ElementID id)
-    NodeCollection<Node>::iterator find_by_id(ElementID id)
-    NodeCollection<Node>::const_iterator {query} find_by_id(ElementID id)
-    NodeCollection<Ramp>::const_iterator {query} ramp_cbegin()
-    NodeCollection<Ramp>::const_iterator {query} ramp_cend()
-    NodeCollection<Workers>::const_iterator {query} ramp_cbegin()
-    NodeCollection<Workers>::const_iterator {query} ramp_cend()
-    NodeCollection<Storehouse>::const_iterator {query} ramp_cbegin()
-    NodeCollection<Sotrehouse>::const_iterator {query} ramp_cend()
-private:
+    Factory() = default;
 
+    void add_ramp(Ramp&& ramp) { ramps_.add(std::move(ramp)); }
+    void remove_ramp(ElementID id) { ramps_.remove_by_id(id); }
+    NodeCollection<Ramp>::iterator find_ramp_by_id(ElementID id) { return ramps_.find_by_id(id); }
+    NodeCollection<Ramp>::const_iterator find_ramp_by_id(ElementID id) const { return ramps_.find_by_id(id); }
+    NodeCollection<Ramp>::const_iterator ramp_cbegin() const { return ramps_.cbegin(); }
+    NodeCollection<Ramp>::const_iterator ramp_cend() const { return ramps_.cend(); }
+
+
+    void add_storehouse(Storehouse&& storehouse) { storehouses_.add(std::move(storehouse)); }
+    void remove_storehouse(ElementID id);
+    NodeCollection<Storehouse>::iterator find_storehouse_by_id(ElementID id) { return storehouses_.find_by_id(id); }
+    NodeCollection<Storehouse>::const_iterator find_storehouse_by_id(ElementID id) const { return storehouses_.find_by_id(id); }
+    NodeCollection<Storehouse>::const_iterator storehouse_cbegin() const { return storehouses_.cbegin(); }
+    NodeCollection<Storehouse>::const_iterator storehouse_cend() const { return storehouses_.cend(); }
+
+
+    void add_worker(Worker&& worker) { workers_.add(std::move(worker)); }
+    void remove_worker(ElementID id);
+    NodeCollection<Worker>::iterator find_worker_by_id(ElementID id) { return workers_.find_by_id(id); }
+    NodeCollection<Worker>::const_iterator find_worker_by_id(ElementID id) const { return workers_.find_by_id(id); }
+    NodeCollection<Worker>::const_iterator worker_cbegin() const { return workers_.cbegin(); }
+    NodeCollection<Worker>::const_iterator worker_cend() const { return workers_.cend(); }
+
+    enum class NodeColor { UNVISITED, VISITED, VERIFIED };
+    bool has_reachable_storehouse(const PackageSender* sender, std::map<const PackageSender*, NodeColor>& node_colors);
+    bool is_consistent();
+
+    void do_deliveries(Time time);
+    void do_package_passing();
+    void do_work(Time time);
+
+    void remove_receiver(NodeCollection<Worker>& collection, ElementID id);
+    void remove_receiver(NodeCollection<Storehouse>& collection, ElementID id);
+
+private:
+    NodeCollection<Ramp> ramps_;
+    NodeCollection<Storehouse> storehouses_;
+    NodeCollection<Worker> workers_;
 };
+
 
 #endif //NETSIM_FACTORY_HPP
