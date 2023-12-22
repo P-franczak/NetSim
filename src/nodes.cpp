4779b2f7f008c184275be0603cd37f3dfef72b5b
P-franczak <147486694+P-franczak@users.noreply.github.com>
2023-12-22 22:55:59 +0100

    Update nodes.cpp

--- src/nodes.cpp
+++ src/nodes.cpp
@@ -52,19 +52,35 @@ void PackageSender::send_package() {
     }
 }
 
+void PackageSender::push_package(Package&& package){
+    if (!bufor_.has_value()) {
+        bufor_.emplace(std::move(package));
+    }
+}
+
+Worker::Worker(const Worker &worker){
+    id_ = worker.get_id();
+    pd_ = worker.get_processing_duration();
+    receiver_preferences_ = worker.receiver_preferences_;
+    q_ = std::make_unique<PackageQueue>(worker.get_queue()->get_queue_type());
+}
+
+Worker& Worker::operator=(const Worker &worker) noexcept {
+    id_ = worker.get_id();
+    pd_ = worker.get_processing_duration();
+    receiver_preferences_ = worker.receiver_preferences_;
+    q_ = std::make_unique<PackageQueue>(worker.get_queue()->get_queue_type());
+    return *this;
+}
+
 void Worker::do_work(Time t) {
-    if (!bufor_ && !q_->empty()) {
+    if(!bufor_.has_value()){
         bufor_.emplace(q_->pop());
         t_ = t;
     }
-    else {
-        if (t - t_ + 1 == pd_) {
-            push_package(Package(bufor_.value().get_id()));
-            bufor_.reset();
-            if (!q_->empty()) {
-                bufor_.emplace(q_->pop());
-            }
-        }
+    else if(t - t_ + 1 == pd_){
+        push_package(std::move(*bufor_));
+        bufor_.reset();
     }
 }
 
@@ -76,15 +92,22 @@ void Storehouse::receive_package(Package &&p) {
     d_->push(std::move(p));
 }
 
+Ramp::Ramp(const Ramp &ramp) {
+    id_ = ramp.get_id();
+    di_ = ramp.get_delivery_interval();
+    receiver_preferences_ = ramp.receiver_preferences_;
+}
+
+Ramp& Ramp::operator=(const Ramp &ramp) noexcept{
+    id_ = ramp.get_id();
+    di_ = ramp.get_delivery_interval();
+    receiver_preferences_ = ramp.receiver_preferences_;
+    return *this;
+}
+
 void Ramp::deliver_goods(Time t) {
-    if (!bufor_) {
-        push_package(Package());
-        bufor_.emplace(id_);
-        t_ = t;
-    }
-    else {
-        if (t - di_ == t_) {
-            push_package(Package());
-        }
+    if (t % di_ == 1){
+        Package package = Package();
+        push_package(std::move(package));
     }
 }
